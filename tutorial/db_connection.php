5339a94955712bc0c805c0b98155bd190954a1f9
Yuri Reyes <109398894+YuriKun11@users.noreply.github.com>
2024-09-22 03:47:39 +0800

    db_connection.php

--- tutorial/db_connection.php
+++ tutorial/db_connection.php
@@ -6,7 +6,8 @@ $password = "";
 $dbname = "tutorialbase";  //Ito lang papalitan mo
 
 $conn = mysqli_connect($servername, $username, $password, $dbname);
-
+//There are two types of checking $conn
+//The standard way is $conn->connect_error
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
